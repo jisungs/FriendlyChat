1550f68ce112f1964897ccb8f7ee4a20c4252b8b
jisung <jisung.smart@gmail.com>
2018-10-14 19:36:21 +0900

    Initial commit`

---
+++ FriendlyChat/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FriendlyChat
+//
+//  Created by The book on 2018. 10. 14..
+//  Copyright © 2018년 The book. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
